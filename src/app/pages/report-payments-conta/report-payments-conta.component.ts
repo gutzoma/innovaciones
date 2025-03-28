792b9d400fb5acacf0da62ca313435164542120d
Omar <gutzoma@gmail.com>
2025-03-27 20:07:27 -0600

    add report-payments-conta and searchs, add kfc

--- src/app/pages/report-payments-conta/report-payments-conta.component.ts
+++ src/app/pages/report-payments-conta/report-payments-conta.component.ts
@@ -1,12 +1,157 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { GeneralesService } from '../../_services/generales.service';
+import { formatDate } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { SelectDropDownModule } from 'ngx-select-dropdown';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { Report } from '../../_models/report';
+import * as ExcelJS from 'exceljs';
 
+import {
+  NativeDateAdapter,
+  DateAdapter,
+  MAT_DATE_FORMATS
+} from '@angular/material/core';
+import { ReportsService } from '../../_services/reports.service';
+
+declare let $: any;
+
+export const PICK_FORMATS = {
+  parse: { dateInput: { month: 'short', year: 'numeric', day: 'numeric' } },
+  display: {
+    dateInput: 'input',
+    monthYearLabel: { year: 'numeric', month: 'short' },
+    dateA11yLabel: { year: 'numeric', month: 'long', day: 'numeric' },
+    monthYearA11yLabel: { year: 'numeric', month: 'long' },
+  },
+};
+
+class PickDateAdapter extends NativeDateAdapter {
+  override format(date: Date, displayFormat: Object): string {
+    if (displayFormat === 'input') {
+      return formatDate(date, 'dd-MM-yyyy', this.locale);
+    } else {
+      return date.toDateString();
+    }
+  }
+}
 @Component({
   selector: 'app-report-payments-conta',
-  imports: [],
+  standalone: true,
+  imports: [
+    FormsModule,
+    CommonModule,
+    SelectDropDownModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './report-payments-conta.component.html',
   styles: ``,
-  changeDetection: ChangeDetectionStrategy.OnPush
+  providers: [
+    GeneralesService,
+    ReportsService,
+    { provide: DateAdapter, useClass: PickDateAdapter },
+    { provide: MAT_DATE_FORMATS, useValue: PICK_FORMATS }
+  ],
 })
+
 export class ReportPaymentsContaComponent {
+
+  public asesores!: any;
+  public report: Report;
+  public reportInfo!: any;
+
+  constructor(private _generalesservice: GeneralesService, private _reportsservice: ReportsService) {
+    
+    this.report = new Report('', '', '', '');
+   }
+
+   date : any;
+
+   ngOnInit(): void {
+     this.getAsesores();
+   }
+
+   getAsesores() {
+    this._generalesservice.getAsesores().subscribe(
+      response => {
+        if (response) {
+          this.asesores = response;
+        }
+      },
+      error => {
+        console.log(<any>error);
+      }
+    );
+  }
+
+  getReportPayments(form: { reset: () => void; }){
+    if($("input[name=f_inicial]").val() == '' || $("input[name=f_final]").val() == '') {
+     alert('ingresa fechas validas')
+    }else{
+      var f_inicial:any = $("input[name=f_inicial]").val();
+      f_inicial = f_inicial.split("-");
+      this.report.f_inicial = f_inicial[2]+'-'+f_inicial[1]+'-'+f_inicial[0];
+      var f_final:any = $("input[name=f_final]").val();
+      f_final = f_final.split("-");
+      this.report.f_final = f_final[2]+'-'+f_final[1]+'-'+f_final[0];
+      this._reportsservice.getReportPayments(this.report).subscribe(
+        response => {
+          if (response) {
+            if(response == 'No existen'){
+              this.reportInfo = [{'cliente_id': 'No hay datos'}];
+            }else{
+              this.reportInfo = response;
+              $(".print-report").removeClass("disp-n");
+            }
+            setTimeout(() => {
+              $('input').select();
+            }, 300);
+          }
+        },
+        error => {
+          console.log(<any>error);
+          if (error.status === 401) {
+            localStorage.clear();
+            window.location.href = '';
+          }
+        }
+      );
+    }
+  }
+
+  descargar(): void {
+    const workbook = new ExcelJS.Workbook();
+    const worksheet = workbook.addWorksheet('Tabla');
+
+    const table:any = document.getElementById('miTabla');
+    const rows:any = table.querySelectorAll('tr');
+    rows.forEach((row: { querySelectorAll: (arg0: string) => any[]; }) => {
+      const rowData: any[] = [];
+      row.querySelectorAll('th, td').forEach((cell: { textContent: string; }) => {
+        rowData.push(cell.textContent.trim());
+      });
+      worksheet.addRow(rowData);
+    });
+
+    workbook.xlsx.writeBuffer().then(buffer => {
+      this.saveExcelFile(buffer, 'tabla.xlsx');
+    });
+  }
+  saveExcelFile(buffer: ArrayBuffer, fileName: string): void {
+    const data = new Blob([buffer], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
+    const url = window.URL.createObjectURL(data);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = fileName;
+    a.click();
+    window.URL.revokeObjectURL(url);
+  }
+
 
 }
--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -1,12 +1,139 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component } from '@angular/core';
+import { SearchService } from '../../_services/search.service';
+import { SelectDropDownModule } from 'ngx-select-dropdown';
+import { TabsModule} from 'ngx-bootstrap/tabs';
+import { CommonModule } from '@angular/common';
 
+declare let $: any;
 @Component({
   selector: 'app-search',
-  imports: [],
+  imports: [SelectDropDownModule,TabsModule, CommonModule],
   templateUrl: './search.component.html',
   styles: ``,
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  providers:[SearchService]
 })
 export class SearchComponent {
 
+  public clientes: any;
+  public id_search: any;
+  public clienteInfo: any;
+  public pagos: any;
+  public save_credito: any;
+	public status!: string;
+  public datos_credito!: any;
+  public pagos_sum!: any;
+
+overlay = false;
+public config!:any;
+
+  constructor(
+    private _searchservice: SearchService,
+    private cdr: ChangeDetectorRef
+  ) {
+    this.id_search = '';
+    this.config = {
+      displayKey: "name",
+      search: true,
+      searchPlaceholder:'Buscar Cliente',
+      clearOnSelection: true
+    };
+  }
+  ngOnInit(): void {
+    this.getSearch();
+    this.clienteInfo ={'nombres':'','materno':''}
+    this.pagos ={}
+
+  }
+
+  onSelectionChange(event: any) {
+    this.id_search = event.value.id;
+    this.getCliente(this.id_search); 
+    this.getClienteCredito(this.id_search); 
+    $('#showSearch').removeClass('disp-n');
+  }
+
+  getSearch() {
+    var data = new Array();
+    this._searchservice.getSearch().subscribe(
+      response => {
+        if (response != 'No existen') {
+          response.forEach(function (cliente: any) {
+            data.push({
+              'id': cliente.cliente_id,
+              'name': cliente.cliente_id + ' / ' + cliente.nombres + ' ' + cliente.paterno + ' ' + cliente.materno + ' / ' + cliente.curp
+            });
+          });
+          this.clientes = data;
+        }
+      },
+      error => {
+        console.log(<any>error);
+        if (error.status === 401) {
+          localStorage.clear();
+          window.location.href = '';
+        }
+      }
+    );
+  }
+
+  getCliente(cliente: number) {
+    this._searchservice.getClienteSearch(cliente).subscribe(
+      response => {
+        if (response) {
+          
+          this.clienteInfo = response[0];
+          setTimeout(() => {
+            $("#showSearch").removeClass('disp_n');
+          }, 300);
+          this.cdr.detectChanges();
+        }
+      },
+      error => {
+        console.log(<any>error);
+        if (error.status === 401) {
+          localStorage.clear();
+          window.location.href = '';
+        }
+      }
+    );
+  }
+  getClienteCredito(cliente: number) {
+    this._searchservice.getClienteCreditoSearch(cliente).subscribe(
+      response => {
+        if (response != 'No existen') {
+
+          this.datos_credito = response;
+
+          let total=0;
+          this.datos_credito.forEach(function(a:any){total += parseFloat(a.deposito);});
+          this.pagos_sum = total;
+
+        }else{
+          this.pagos_sum = 0;
+          this.datos_credito = [{
+            no_pago: 'No', cuota: 'Disponible', fecha: '', fecha_pago: null, pagado: ''
+          }]
+        }
+        this.cdr.detectChanges();
+      },
+      error => {
+        console.log(<any>error);
+      }
+    );
+  }
+  printPage() {
+    let printContents, popupWin;
+    printContents = $('#agrrement-section').html();
+    popupWin = window.open('', '_blank', 'top=0,left=0,height=100%,width=auto');
+    if (popupWin) {
+      popupWin.document.open();
+      popupWin.document.write(`
+				<html>
+            <body onload="window.print();window.close()" style="font-size: 12px;">${printContents}</body>
+        </html>
+      `);
+      popupWin.document.close();
+    }
+  }
 }
