1f786e24fc397bb4fc2bdf3f807c3829d9bcfc5d
Omar <gutzoma@gmail.com>
2025-04-01 09:39:13 -0600

    add images searchs component

--- src/app/_services/global.ts
+++ src/app/_services/global.ts
@@ -1,11 +1,11 @@
-// export var Global = {
-
-//     url:'http://paayito.local/finanback/api/'
-
-// }
-
 export var Global = {
 
-    url:'https://adminqa0.paayito.com/backqa/api/'
+    url:'http://paayito.local/finanback/api/'
 
- }
+}
+
+// export var Global = {
+
+//     url:'https://adminqa0.paayito.com/backqa/api/'
+
+//  }
